fea51a73df2ebdd4e020e5ce69520e8114bf9c48
Yashaswini k <yashaswini@getvymo.com>
2023-04-03 15:02:03 +0530

    add to one class to review

---
+++ src/test/java/Sridharscript/Tester.java
@@ -0,0 +1,14 @@
+package Sridharscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tester 
+{
+	@Test
+	public void tester()
+	{
+		Reporter.log("hi",true);
+	}
+
+}
