d33c1504ef110e457619d01d3a2d9acc93df1d50
Sridharkgmba <121252309+Sridharkgmba@users.noreply.github.com>
2023-04-04 11:44:58 +0530

    Update FigureRice.java

--- src/test/java/BiseBaleBatha/FigureRice.java
+++ src/test/java/BiseBaleBatha/FigureRice.java
@@ -2,8 +2,10 @@ package BiseBaleBatha;
 
 public class FigureRice 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("i am gping");
+		System.out.println("i am coming to house");
 	}
 
 }
